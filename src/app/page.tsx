5a1f3eb125a366022b0e7094bb5b6ef28097dbd4
ShubHH03 <mak.shubham2003@gmail.com>
2025-08-29 13:39:40 +0530

    add Navbar component with dropdown menu for courses and contact links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-
-
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Hello World</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      {/* <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer /> */}
+    </main>
   );
 }
